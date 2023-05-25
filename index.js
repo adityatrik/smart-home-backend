86d89187ac4cb0e1bb4600d555fe53791c575b19
aditya tri k <adityatrik1@gmail.com>
2023-05-25 19:56:54 +0700

    update 1 commit

--- index.js
+++ index.js
@@ -6,13 +6,31 @@ const server = http.createServer((req, res) => {
     const queryParams = url.searchParams;
 
     // Ambil nilai parameter
-    const name = queryParams.get('name');
-    const age = queryParams.get('age');
+    // Req : ?id=1&t=260&h=35&pir=0&sw=0&g=200&as=200&al=1
+    const id = queryParams.get('id');
+    // console.log(id);
+    const t = queryParams.get('t');
+    // console.log(t);
+    const h = queryParams.get('h');
+    // console.log(h);
+    const pir = queryParams.get('pir');
+    // console.log(pir);
+    const sw = queryParams.get('sw');
+    // console.log(sw);
+    const g = queryParams.get('g');
+    // console.log(g);
+    const as = queryParams.get('as');
+    // console.log(as);
+    const al = queryParams.get('al');
+    // console.log(al);
 
     // Berikan respon
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(`Hello, ${name}! Your age is ${age}.`);
+    if (id !== null) {
+      console.log(`{"id":"${id}","t":"${t}","h":"${h}","pir":"${pir}","sw":"${sw}","g":"${g}","as":"${as}","as":"${al}"}`);
+    }
+    res.end(`{"id":"1","l1":"100","l2":"100","l3":"100","l4":"100","l5":"0"}`);
   } else {
     res.statusCode = 404;
     res.end('Not Found');

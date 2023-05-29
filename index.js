66fdb0067bb8badc6a75cf9ffef6f3419ed74537
aditya tri k <adityatrik1@gmail.com>
2023-05-30 00:06:11 +0700

    mqtt  commit

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
-const http = require('http');
+const mqtt = require('mqtt');
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    const queryParams = url.searchParams;
+// Konfigurasi MQTT broker
+const brokerUrl = 'mqtt://103.146.203.230';  // Ganti dengan URL broker yang sesuai
+const topic = '/mcv/sh/device/1';  // Ganti dengan topik yang ingin Anda subscribe
 
-    // Ambil nilai parameter
-    // Req : ?id=1&t=260&h=35&pir=0&sw=0&g=200&as=200&al=1
-    const id = queryParams.get('id');
-    // console.log(id);
-    const t = queryParams.get('t');
-    // console.log(t);
-    const h = queryParams.get('h');
-    // console.log(h);
-    const pir = queryParams.get('pir');
-    // console.log(pir);
-    const sw = queryParams.get('sw');
-    // console.log(sw);
-    const g = queryParams.get('g');
-    // console.log(g);
-    const as = queryParams.get('as');
-    // console.log(as);
-    const al = queryParams.get('al');
-    // console.log(al);
+// Membuat koneksi MQTT
+const client = mqtt.connect(brokerUrl);
 
-    // Berikan respon
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    if (id !== null) {
-      console.log(`{"id":"${id}","t":"${t}","h":"${h}","pir":"${pir}","sw":"${sw}","g":"${g}","as":"${as}","as":"${al}"}`);
+// Event listener saat koneksi berhasil dibuat
+client.on('connect', () => {
+  console.log('Terhubung ke MQTT broker');
+  
+  // Melakukan subscribe pada topik yang ditentukan
+  client.subscribe(topic, (err) => {
+    if (err) {
+      console.error('Gagal melakukan subscribe:', err);
+    } else {
+      console.log('Berhasil melakukan subscribe pada topik:', topic);
     }
-    res.end(`{"id":"1","l1":"100","l2":"100","l3":"100","l4":"100","l5":"0"}`);
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found');
-  }
+  });
 });
 
-const port = 3002;
-server.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+// Event listener saat menerima pesan
+client.on('message', (topic, message) => {
+  console.log('Menerima pesan:', message.toString());
+  // Lakukan tindakan lain sesuai kebutuhan dengan pesan yang diterima
+});
+
+// Event listener saat koneksi terputus
+client.on('close', () => {
+  console.log('Koneksi ke MQTT broker terputus');
+});
+
+// Event listener saat terjadi error
+client.on('error', (err) => {
+  console.error('Error:', err);
 });
